244f6d399a64541d3ff6410b1dcc3b2d9b1c0ed4
mil <miles.sandlar@gmail.com>
2013-02-24 20:32:52 -0500

    Add SOL Engage to ui folder

---
+++ ui/sol-engage/js/font-replace.js
@@ -0,0 +1,28 @@
+Cufon.replace('h1');
+Cufon.replace('h1 span');
+Cufon.replace('h2');
+Cufon.replace('h3');
+Cufon.replace('h4');
+Cufon.replace('h5');
+Cufon.replace('.forum div');
+Cufon.replace('#ask');
+
+
+Cufon.replace('div.percentage');
+Cufon.replace('div.percentage span');
+Cufon.replace('div.percentage div.label');
+Cufon.replace('div.summary');
+Cufon.replace('#footer');
+
+
+
+
+
+Cufon.replace('input#name');
+
+Cufon.replace('#navigation li a', {hover:true});
+Cufon.replace('.section a', {hover:true});
+Cufon.replace('.section span', {hover:true});
+/*Cufon.replace('.content .test .questions .question p')*/
+
+
