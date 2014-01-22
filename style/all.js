2d13f7ea55dc2e68095a8e6e22a3fc2ce7bec0df
mil <miles.sandlar@gmail.com>
2014-01-22 07:27:30 -0500

    remove old dependencies

--- style/all.js
+++ style/all.js
@@ -1,9 +1,6 @@
 ---
 
 ---
-{% include style/js/cufon-yui.js %}
-{% include style/js/helvetiker.font.js %}
-{% include style/js/font-replace.js %}
 {% include style/js/FancyZoom.js %}
 {% include style/js/FancyZoomHTML.js %}
 {% include style/js/sh_main.js %}
